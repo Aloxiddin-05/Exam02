5890a55a9551b0af3f8b5968d4466e59801a2d3f
Aloxiddin-05 <aloxiddinyogmirov1@gmail.com>
2025-07-07 14:22:07 +0500

    bajarildi

--- task01.py
+++ task01.py
@@ -1,14 +1,14 @@
-def add(a, b):
+def add(a: int,b: int):
     return a + b
     
     
-def subtract(a, b):
+def subtract(a: int, b: int):
     return a - b
 
-def multiply(a, b):
+def multiply(a: int, b: int):
     return a * b
     
-def divide(a, b):
+def divide(a: int, b: int):
     return a / b
 
 def main():
